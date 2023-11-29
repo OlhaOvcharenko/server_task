e7c6f4b4b04f228173d8bdf95b104a61285c4306
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-29 21:35:34 +0300

    change server.js

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const app = express();
 
 app.use((req, res, next) => {
   res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
+    res.sendFile(path.join(__dirname, `/views/${name}/`));
   };
   next();
 });
-
-app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user/', (req, res, next) => {
   res.show('forbidden.html');;
@@ -24,9 +22,12 @@ app.get('/about', (req, res) => {
   res.show('about.html')
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 app.use((req, res) => {
-  res.status(404).show('404.html');;
-})
+  res.status(404).show('404.html');
+});
    
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
